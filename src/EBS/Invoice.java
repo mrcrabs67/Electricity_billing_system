8183d315357923ab43c4466b2f7feac04ffda3d5
“Viktor <Viktor.ivaschenkov@raiffeisen.ru>
2022-08-01 17:19:59 +0300

    Начал создавать класс

---
+++ src/EBS/Invoice.java
@@ -0,0 +1,29 @@
+package EBS;
+
+public class Invoice {
+
+    int initialReadings;
+    int finalReadings;
+    int sum;
+
+    public enum month {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+    public void PrintStart(){
+        System.out.println("Start");
+    }
+
+
+}
--- src/EBS/Main.java
+++ src/EBS/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Go!");
+
+        Invoice invoice = new Invoice();
+        invoice.PrintStart();
     }
 }
